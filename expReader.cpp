be6486bc2664bcfcd1d030b5911f2a0fb97c08ec
Alexander Giang <alexgiang96@gmail.com>
2017-04-29 22:27:24 -0700

    uploading expression Reader

---
+++ expReader.cpp
@@ -0,0 +1,19 @@
+/* Members: Alexander Giang, Shawn Chua
+ * Class Users: cssc0885, cssc0894
+ * REDID: 818285579, 817662151
+ * Class Information: CS530, Spring 2017
+ * Assignment #3, Assignment/Expression Parser
+ * Filename: expReader.cpp
+ */
+#include "expReader.h"
+#include <string.h>
+#include <fstream>
+#include <iostream>
+#include <exception>
+#include <valarray>
+using namespace std;
+
+expReader::isExp(string input){
+
+    //TO DO
+}
