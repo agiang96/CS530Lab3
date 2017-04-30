111796de0abc08acaaf33c078fb712d975e65d38
nibichua <nibichua@gmail.com>
2017-04-29 20:36:41 -0700

    Update header template

--- readFile.h
+++ readFile.h
@@ -2,7 +2,7 @@
  * Class Users: cssc0885, cssc0894
  * REDID: 818285579, 817662151
  * Class Information: CS530, Spring 2017
- * Assignment #3, Parser
+ * Assignment #3, Assignment/Expression Parser
  * Filename: readFile.h
  */
 
